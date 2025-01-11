a7421ffd134cc3587400a24525df32cdcd4cd983
Manmeet Gill <msgillus@gmail.com>
2025-01-10 16:53:28 -0800

    fixed footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,167 +1,47 @@
 import { Row, Col } from "antd";
-import { withTranslation, TFunction } from "react-i18next";
-import { SvgIcon } from "../../common/SvgIcon";
-import Container from "../../common/Container";
+import { FooterWrapper, Title, Link, SocialIcons, IconWrapper } from "./styles";
 
-import i18n from "i18next";
-import {
-  FooterSection,
-  Title,
-  NavLink,
-  Extra,
-  LogoContainer,
-  Para,
-  Large,
-  Chat,
-  Empty,
-  FooterContainer,
-  Language,
-  Label,
-  LanguageSwitch,
-  LanguageSwitchContainer,
-} from "./styles";
-
-interface SocialLinkProps {
-  href: string;
-  src: string;
-}
-
-const Footer = ({ t }: { t: TFunction }) => {
-  const handleChange = (language: string) => {
-    i18n.changeLanguage(language);
-  };
-
-  const SocialLink = ({ href, src }: SocialLinkProps) => {
-    return (
-      <a
-        href={href}
-        target="_blank"
-        rel="noopener noreferrer"
-        key={src}
-        aria-label={src}
-      >
-        <SvgIcon src={src} width="25px" height="25px" />
-      </a>
-    );
-  };
-
+const Footer = () => {
   return (
-    <>
-      <FooterSection>
-        <Container>
-          <Row justify="space-between">
-            <Col lg={10} md={10} sm={12} xs={12}>
-              <Language>{t("Contact")}</Language>
-              <Large to="/">{t("Tell us everything")}</Large>
-              <Para>
-                {t(`Do you have any question? Feel free to reach out.`)}
-              </Para>
-              <a href="mailto:l.qqbadze@gmail.com">
-                <Chat>{t(`Let's Chat`)}</Chat>
-              </a>
-            </Col>
-            <Col lg={8} md={8} sm={12} xs={12}>
-              <Title>{t("Policy")}</Title>
-              <Large to="/">{t("Application Security")}</Large>
-              <Large to="/">{t("Software Principles")}</Large>
-            </Col>
-            <Col lg={6} md={6} sm={12} xs={12}>
-              <Empty />
-              <Large to="/">{t("Support Center")}</Large>
-              <Large to="/">{t("Customer Support")}</Large>
-            </Col>
-          </Row>
-          <Row justify="space-between">
-            <Col lg={10} md={10} sm={12} xs={12}>
-              <Empty />
-              <Language>{t("Address")}</Language>
-              <Para>Rancho Santa Margarita</Para>
-              <Para>2131 Elk Street</Para>
-              <Para>California</Para>
-            </Col>
-            <Col lg={8} md={8} sm={12} xs={12}>
-              <Title>{t("Company")}</Title>
-              <Large to="/">{t("About")}</Large>
-              <Large to="/">{t("Blog")}</Large>
-              <Large to="/">{t("Press")}</Large>
-              <Large to="/">{t("Careers & Culture")}</Large>
-            </Col>
-            <Col lg={6} md={6} sm={12} xs={12}>
-              <Label htmlFor="select-lang">{t("Language")}</Label>
-              <LanguageSwitchContainer>
-                <LanguageSwitch onClick={() => handleChange("en")}>
-                  <SvgIcon
-                    src="united-states.svg"
-                    aria-label="homepage"
-                    width="30px"
-                    height="30px"
-                  />
-                </LanguageSwitch>
-                <LanguageSwitch onClick={() => handleChange("es")}>
-                  <SvgIcon
-                    src="spain.svg"
-                    aria-label="homepage"
-                    width="30px"
-                    height="30px"
-                  />
-                </LanguageSwitch>
-              </LanguageSwitchContainer>
-            </Col>
-          </Row>
-        </Container>
-      </FooterSection>
-      <Extra>
-        <Container border={true}>
-          <Row
-            justify="space-between"
-            align="middle"
-            style={{ paddingTop: "3rem" }}
-          >
-            <NavLink to="/">
-              <LogoContainer>
-                <SvgIcon
-                  src="logo.svg"
-                  aria-label="homepage"
-                  width="101px"
-                  height="64px"
-                />
-              </LogoContainer>
-            </NavLink>
-            <FooterContainer>
-              <SocialLink
-                href="https://github.com/Adrinlol/create-react-app-adrinlol"
-                src="github.svg"
-              />
-              <SocialLink
-                href="https://twitter.com/Adrinlolx"
-                src="twitter.svg"
-              />
-              <SocialLink
-                href="https://www.linkedin.com/in/lasha-kakabadze/"
-                src="linkedin.svg"
-              />
-              <SocialLink
-                href="https://medium.com/@lashakakabadze/"
-                src="medium.svg"
-              />
-              <a
-                href="https://ko-fi.com/Y8Y7H8BNJ"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img
-                  height="36"
-                  style={{ border: 0, height: 36 }}
-                  src="https://storage.ko-fi.com/cdn/kofi3.png?v=3"
-                  alt="Buy Me a Coffee at ko-fi.com"
-                />
-              </a>
-            </FooterContainer>
-          </Row>
-        </Container>
-      </Extra>
-    </>
+    <FooterWrapper>
+      <Row justify="space-between">
+        <Col lg={6} md={12} sm={24}>
+          <Title>Contact</Title>
+          <p>We'd love to hear from you! Feel free to reach out.</p>
+          <Link href="mailto:info@punjabsoft.com">info@punjabsoft.com</Link>
+        </Col>
+
+        <Col lg={6} md={12} sm={24}>
+          <Title>Address</Title>
+          <p>PunjabSoft Limited</p>
+          <p>Patiala, Punjab</p>
+          <p>India</p>
+        </Col>
+
+        <Col lg={6} md={12} sm={24}>
+          <Title>Quick Links</Title>
+          <Link href="/">About Us</Link>
+          <Link href="/">Careers</Link>
+          <Link href="/privacy">Privacy Policy</Link>
+        </Col>
+
+        <Col lg={6} md={12} sm={24}>
+          <Title>Follow Us</Title>
+          <SocialIcons>
+            <IconWrapper href="https://github.com/" target="_blank">
+              <img src="../../../img/svg/github.svg" alt="GitHub" />
+            </IconWrapper>
+            <IconWrapper href="https://twitter.com/" target="_blank">
+              <img src="../../../img/svg/twitter.svg" alt="Twitter" />
+            </IconWrapper>
+            <IconWrapper href="https://linkedin.com/" target="_blank">
+              <img src="../../../img/svg/linkedin.svg" alt="LinkedIn" />
+            </IconWrapper>
+          </SocialIcons>
+        </Col>
+      </Row>
+    </FooterWrapper>
   );
 };
 
-export default withTranslation()(Footer);
+export default Footer;
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,170 +1,40 @@
 import styled from "styled-components";
-import { Link } from "react-router-dom";
 
-export const FooterSection = styled("footer")`
-  background: rgb(241, 242, 243);
-  padding: 2.5rem 0;
+export const FooterWrapper = styled.div`
+  background-color: #f8f9fa;
+  padding: 40px 20px;
+  text-align: left;
 `;
 
-export const Title = styled("h4")`
-  font-size: 22px;
-  text-transform: capitalize;
-  color: #18216d;
-
-  @media screen and (max-width: 414px) {
-    padding: 1.5rem 0;
-  }
+export const Title = styled.h3`
+  font-size: 18px;
+  color: #212529;
+  margin-bottom: 15px;
 `;
 
-export const NavLink = styled(Link)`
+export const Link = styled.a`
   display: block;
-  font-size: 1rem;
-  margin-bottom: 0.625rem;
-  transition: all 0.2s ease-in-out;
-
-  &:hover,
-  &:active,
-  &:focus {
-    color: #15418e;
-  }
-`;
-
-export const Extra = styled("section")`
-  background: rgb(241, 242, 243);
-  position: relative;
-  width: 100%;
-  margin-right: auto;
-  margin-left: auto;
-  padding-bottom: 2rem;
-`;
-
-export const LogoContainer = styled("div")`
-  display: flex;
-  position: relative;
-`;
-
-export const Para = styled("div")`
-  color: #18216d;
-  font-size: 14px;
-  width: 70%;
-`;
-
-export const Large = styled(Link)`
-  font-size: 16px;
-  color: #000;
-  cursor: pointer;
-  transition: all 0.2s ease-in-out;
-  text-transform: capitalize;
-  line-height: 24px;
-  display: block;
-  margin-bottom: 0.625rem;
-  transition: all 0.3s ease-in-out;
-  max-width: max-content;
+  color: #007bff;
+  text-decoration: none;
+  margin-bottom: 8px;
 
   &:hover {
-    color: rgb(255, 130, 92);
-    text-underline-position: under;
-    text-decoration: rgb(255, 130, 92) wavy underline;
+    text-decoration: underline;
   }
 `;
 
-export const Chat = styled("p")`
-  color: #18216d;
-  max-width: fit-content;
-  border-bottom: 1px solid #18216d;
-  cursor: pointer;
-  margin-top: 1rem;
-  transition: all 0.3s ease-in-out;
-
-  &:hover {
-    border-bottom: 1px solid rgb(255, 130, 92);
-    color: rgb(255, 130, 92);
-  }
-`;
-
-export const Empty = styled("div")`
-  position: relative;
-  height: 53px;
-`;
-
-export const FooterContainer = styled("div")`
-  max-width: 510px;
-  width: 100%;
+export const SocialIcons = styled.div`
   display: flex;
-  justify-content: space-between;
-  text-align: center;
-  align-items: center;
-  transition: all 0.1s ease-in-out;
-
-  a {
-    &:hover,
-    &:active,
-    &:focus {
-      -webkit-transform: scale(1.1);
-      -ms-transform: scale(1.1);
-      transform: scale(1.1);
-    }
-  }
-
-  @media screen and (max-width: 769px) {
-    width: auto;
-
-    a:not(:last-child) {
-      display: none;
-    }
-  }
-
-  div {
-    cursor: pointer;
-    margin-right: 15px;
-    width: 25px;
-    height: 25px;
-
-    &:hover {
-      fill: rgb(255, 130, 92);
-    }
-  }
+  gap: 15px;
+  margin-top: 15px;
 `;
 
-export const Language = styled("h4")`
-  font-size: 22px;
-  text-transform: capitalize;
-  color: #18216d;
+export const IconWrapper = styled.a`
+  width: 30px;
+  height: 30px;
 
-  @media screen and (max-width: 414px) {
-    padding: 1.5rem 0;
+  img {
+    width: 100%;
+    height: 100%;
   }
-`;
-
-export const Label = styled("label")`
-  font-size: 22px;
-  text-transform: capitalize;
-  color: #18216d;
-  display: block;
-  margin-bottom: 2rem;
-  font-family: "Motiva Sans Bold", serif;
-
-  @media screen and (max-width: 414px) {
-    padding: 1.5rem 0;
-    margin-bottom: 1rem;
-  }
-`;
-
-export const LanguageSwitch = styled("div")`
-  cursor: pointer;
-  transition: all 0.1s ease-in-out;
-
-  &:hover,
-  &:active,
-  &:focus {
-    -webkit-transform: scale(1.1);
-    -ms-transform: scale(1.1);
-    transform: scale(1.1);
-  }
-`;
-
-export const LanguageSwitchContainer = styled("div")`
-  display: flex;
-  justify-content: space-between;
-  width: 85px;
 `;
